6d09196d4b4259dbc7a9374ca905c71f13480f01
erratinsilentio <kacper.kondracki@wp.pl>
2022-12-07 16:05:51 +0100

    computer extended classes added

--- 1. Klasy i dziedziczenie/index.js
+++ 1. Klasy i dziedziczenie/index.js
@@ -67,5 +67,87 @@ class Computer {
   }
 }
 
-// const komputer = new Computer(10, 50, 2, 100, 10);
-// komputer.info();
+class SuperComputer extends Computer {
+  constructor(
+    mocZasilacza,
+    predkoscProcesora,
+    iloscRdzeni,
+    plytaGlowna,
+    gpu,
+    iloscProcesorow
+  ) {
+    if (iloscProcesorow <= 0) {
+      throw new error("All of the properties should be positive!");
+    }
+    super(mocZasilacza, predkoscProcesora, iloscRdzeni, plytaGlowna, gpu);
+    this.iloscProcesorow = iloscProcesorow;
+  }
+
+  get iloscProcesorow() {
+    return this.iloscProcesorow;
+  }
+
+  set iloscProcesorow(x) {
+    this._iloscProcesorow = x;
+  }
+
+  info() {
+    console.log(
+      `Komputer o mocy zasilacza ${this._mocZasilacza}W, ilości procesorów równej ${this._iloscProcesorow} i prędkości ${this._predkoscProcesora}Ghz, ilości rdzeni równej ${this._iloscRdzeni}, płycie głównej ${this._plytaGlowna}, i GPU równym ${this._gpu}Ghz`
+    );
+  }
+
+  calculateProcessorsPower() {
+    return (
+      this._iloscProcesorow * (this._iloscRdzeni * this._predkoscProcesora)
+    );
+  }
+
+  timeInCycles(time) {
+    return (
+      time /
+      (this._iloscProcesorow * (this._predkoscProcesora * this._iloscRdzeni))
+    );
+  }
+}
+
+class CryptoMinerStation extends Computer {
+  constructor(
+    mocZasilacza,
+    predkoscProcesora,
+    iloscRdzeni,
+    plytaGlowna,
+    gpu,
+    iloscGpu
+  ) {
+    if (iloscGpu <= 0) {
+      throw new error("All of the properties should be positive!");
+    }
+    super(mocZasilacza, predkoscProcesora, iloscRdzeni, plytaGlowna, gpu);
+    this.iloscGpu = iloscGpu;
+  }
+
+  get iloscGpu() {
+    return this.iloscGpu;
+  }
+
+  set iloscGpu(x) {
+    this._iloscGpu = x;
+  }
+
+  info() {
+    console.log(
+      `Komputer o mocy zasilacza ${this._mocZasilacza}W, prędkości procesora ${this._predkoscProcesora}Ghz, ilości rdzeni równej ${this._iloscRdzeni}, płycie głównej ${this._plytaGlowna}, i ilości GPU równej ${this._iloscGpu} i ${this._gpu}Ghz`
+    );
+  }
+
+  calculateProcessorsPower() {
+    return (
+      this._gpu * this._iloscGpu + this._iloscRdzeni * this._predkoscProcesora
+    );
+  }
+
+  miningTime(time, cost) {
+    return time * cost * this.calculateProcessorsPower();
+  }
+}
